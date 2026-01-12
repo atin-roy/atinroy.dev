a2ac9f8f111f67f83825caa2648618de5686aff5
Atin Roy <atinroy1357@gmail.com>
2026-01-12 19:38:30 +0530

    feat(weather) : add scoped design system (colors, type, spacing, radius)

--- apps/web/src/app/weather/page.tsx
+++ apps/web/src/app/weather/page.tsx
@@ -1,27 +1,3 @@
-import CalendarButton from "@/components/weather/CalendarButton";
-import InfoTab from "@/components/weather/InfoTab";
-import MainCard from "@/components/weather/MainCard";
-import SearchBar from "@/components/weather/SearchBar";
-import SecondaryCard from "@/components/weather/SecondaryCard";
-
 export default function WeatherPage() {
-    return (
-        <div className="flex flex-col gap-5">
-            <div className="flex gap-5 justify-center mt-4">
-                <SearchBar />
-                <CalendarButton />
-            </div>
-            <div className="flex gap-5 justify-center">
-                <MainCard />
-                <MainCard />
-            </div>
-            <div className="">
-                <div className="flex flex-col gap-5 items-center">
-                    <SecondaryCard />
-                    <SecondaryCard />
-                    <SecondaryCard />
-                </div>
-            </div>
-        </div>
-    );
+    return <h1>Weather App</h1>;
 }
